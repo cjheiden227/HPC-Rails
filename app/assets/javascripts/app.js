e19afca9de7f531ebbe747b8383a409e88fbecd6
Chris <chris@chris-GA-990FXA-UD3.(none)>
2015-03-03 22:12:45 -0800

    Layout fixes, contact form update and css changes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     $('.map-link').on('click', function(e){
     	e.preventDefault();
     	address=encodeURIComponent($(this).attr('data-map'));
-    	map= $("<iframe height='500' scrolling='no'  marginheight='0' marginwidth='0' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyCnXy5N52r2BsyqhjIU-T4shbIqgeaf34g&amp;q="+ address + "'></iframe>");
+    	map= $("<iframe height='400' scrolling='no'  marginheight='0' marginwidth='0' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyCnXy5N52r2BsyqhjIU-T4shbIqgeaf34g&amp;q="+ address + "'></iframe>");
     	map.addClass('map-target small-12 columns big-icon');
     	$('.map-target').replaceWith(map);
     });
@@ -21,6 +21,7 @@ $(document).ready(function(){
     $('#new_message').submit(function(e){
      
         if($(this).valid()){
+            $('.form-header').addClass('submitting');
             $('#submit').prop('disabled', 'disabled');
             $('#spinner').show();
             var target = document.getElementById('spinner');
@@ -49,6 +50,7 @@ $(document).ready(function(){
             e.preventDefault();
         }
     });
+
     //Validate the form
     $('#new_message').validate({
         errorPlacement:function(error, element){
