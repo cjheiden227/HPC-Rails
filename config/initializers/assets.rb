d539d7c61dede52c0e1bb8dbb282e8d1fc24a511
Chris <cjheiden227@gmail.com>
2015-03-11 08:44:26 -0700

    Added foundation-icons to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,6 +16,6 @@ Rails.application.config.assets.precompile += %w( foundation.min.js )
 Rails.application.config.assets.precompile += %w( app.js )
 Rails.application.config.assets.precompile += %w( jquery.stickyNavbar.min.js )
 Rails.application.config.assets.precompile += %w( spin.min.js )
-Rails.application.config.assets.precompile += %w( *.ttf )
-Rails.application.config.assets.precompile += %w( *.woff )
+Rails.application.config.assets.precompile += %w( foundation-icons/foundation-icons.ttf )
+Rails.application.config.assets.precompile += %w( foundation-icons/foundation-icons.woff )
 
