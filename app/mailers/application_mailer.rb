508017222780f60bdef3739aa3b3abafd97867c5
Chris Heiden <cjheiden227@gmail.com>
2021-03-25 17:46:47 -0600

    Deleting unnecessary files

--- app/mailers/application_mailer.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: "referrals@hamlinpsychecenter.com"
-  layout 'mailer'
-
-  def message_confirmation(user)
-  	mail(:to=>user.email, :subject=>'Your message to the Hamlin Psyche Center')
-  	
-  end
-end
